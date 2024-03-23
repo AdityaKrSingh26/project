1acd893798bd8fa080b6c72d7f9c874a3966cc46
AdityaKrSingh26 <adityakrsingh2604@gmail.com>
2024-03-24 00:18:16 +0530

    add search page

--- git-clone/src/App.jsx
+++ git-clone/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import LoginPage from "./components/AuthPage/loginPage";
 import Profile from "./components/Profile/Profile";
+import SearchPage from "./components/SearchPage/SearchPage";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
             <Route path="/" element={<Dashboard />} />
             <Route path="/auth" element={<LoginPage />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/search" element={<SearchPage />} />
           </Routes>
         </Router>
       </div>
