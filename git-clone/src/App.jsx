e5651acbc72087f644d708bad388bb166a6f3e1f
AdityaKrSingh26 <adityakrsingh2604@gmail.com>
2024-03-25 00:34:53 +0530

    repo page

--- git-clone/src/App.jsx
+++ git-clone/src/App.jsx
@@ -4,6 +4,7 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import LoginPage from "./components/AuthPage/loginPage";
 import Profile from "./components/Profile/Profile";
 import SearchPage from "./components/SearchPage/SearchPage";
+import RepositriesPage from "./components/RepostriesPage/RepositriesPage";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
             <Route path="/" element={<Dashboard />} />
             <Route path="/auth" element={<LoginPage />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/repo" element={<RepositriesPage />} />
             <Route path="/search" element={<SearchPage />} />
           </Routes>
         </Router>
